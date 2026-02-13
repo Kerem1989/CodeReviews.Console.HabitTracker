f2937b43def0d5783532cb2ae8077b03923d46e2
Kerem Bjälvenäs Tazedal <98690898+Kerem1989@users.noreply.github.com>
2026-02-13 12:24:17 +0100

    First commit

---
+++ Kerem.HabitTracker/Kerem.HabitTracker/Habit.cs
@@ -0,0 +1,16 @@
+﻿namespace Kerem.HabitTracker ;
+
+    public class Habit
+    {
+        private string _name;
+        private DateTime _occurance;
+        
+        public string Name { get; set; }
+        public DateTime Occurance { get; set; }
+        
+        public Habit(string name, DateTime occurance)
+        {
+            this._name = name;
+            this._occurance = occurance;
+        }
+    }
---
+++ Kerem.HabitTracker/Kerem.HabitTracker/HabitService.cs
@@ -0,0 +1,6 @@
+﻿namespace Kerem.HabitTracker ;
+
+    public class HabitService
+    {
+        
+    }
